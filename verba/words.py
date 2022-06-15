febf9a93fbd5b235f5f4ff417fd72d5eee0117fd
Shreyas Mayya <smayya337@gmail.com>
2022-06-15 15:12:12 -0400

    mostly finish out the other parts of speech

--- verba/words.py
+++ verba/words.py
@@ -74,23 +74,67 @@ class Noun(Word):
 class Verb(Word):
     def __init__(self, target, stem, ending, prefix=None, suffix=None, tackon=None):
         super().__init__(target, stem, ending, prefix, suffix, tackon)
+        assert (self.stem["which"], self.stem["variation"]) == (
+            self.ending["which"],
+            self.ending["variation"],
+        ), "The stem and ending of this word are incompatible!"
+        self.word = self.stem["principal parts"][self.ending["key"] - 1]
+        self.word += self.ending["ending"]
+        if self.prefix:
+            self.word = self.prefix["word"] + self.prefix.get("connect", "") + self.word
+        if self.suffix:
+            self.word = self.word + self.suffix.get("connect", "") + self.suffix["word"]
+        if self.tackon:
+            self.word += self.tackon["word"]
 
 
 class Adjective(Word):
     def __init__(self, target, stem, ending, prefix=None, suffix=None, tackon=None):
         super().__init__(target, stem, ending, prefix, suffix, tackon)
+        assert (self.stem["which"], self.stem["variation"]) == (
+            self.ending["which"],
+            self.ending["variation"],
+        ), "The stem and ending of this word are incompatible!"
+        assert Word.gender_matches(
+            self.stem["gender"], self.ending["gender"]
+        ), "The stem and ending of this word are incompatible!"
+        self.word = self.stem["principal parts"][self.ending["key"] - 1]
+        self.word += self.ending["ending"]
+        if self.prefix:
+            self.word = self.prefix["word"] + self.prefix.get("connect", "") + self.word
+        if self.suffix:
+            self.word = self.word + self.suffix.get("connect", "") + self.suffix["word"]
+        if self.tackon:
+            self.word += self.tackon["word"]
+
 
 
 class Adverb(Word):
     def __init__(self, target, stem, ending, prefix=None, suffix=None, tackon=None):
         super().__init__(target, stem, ending, prefix, suffix, tackon)
+        self.word = self.stem["principal parts"][self.ending["key"] - 1]
+        self.word += self.ending["ending"]
+        if self.prefix:
+            self.word = self.prefix["word"] + self.prefix.get("connect", "") + self.word
+        if self.suffix:
+            self.word = self.word + self.suffix.get("connect", "") + self.suffix["word"]
+        if self.tackon:
+            self.word += self.tackon["word"]
 
 
 class Preposition(Word):
     def __init__(
-        self, target, stem, ending=None, prefix=None, suffix=None, tackon=None
+        self, target, stem, ending, prefix=None, suffix=None, tackon=None
     ):
         super().__init__(target, stem, ending, prefix, suffix, tackon)
+        self.word = self.stem["principal parts"][self.ending["key"] - 1]
+        self.word += self.ending["ending"]
+        if self.prefix:
+            self.word = self.prefix["word"] + self.prefix.get("connect", "") + self.word
+        if self.suffix:
+            self.word = self.word + self.suffix.get("connect", "") + self.suffix["word"]
+        if self.tackon:
+            self.word += self.tackon["word"]
 
 
 class Conjunction(Word):
@@ -98,30 +142,133 @@ class Conjunction(Word):
         self, target, stem, ending=None, prefix=None, suffix=None, tackon=None
     ):
         super().__init__(target, stem, ending, prefix, suffix, tackon)
+        self.word = self.stem["principal parts"][self.ending["key"] - 1]
+        self.word += self.ending["ending"]
+        if self.prefix:
+            self.word = self.prefix["word"] + self.prefix.get("connect", "") + self.word
+        if self.suffix:
+            self.word = self.word + self.suffix.get("connect", "") + self.suffix["word"]
+        if self.tackon:
+            self.word += self.tackon["word"]
 
 
 class Interjection(Word):
     def __init__(
-        self, target, stem, ending=None, prefix=None, suffix=None, tackon=None
+        self, target, stem, ending, prefix=None, suffix=None, tackon=None
     ):
         super().__init__(target, stem, ending, prefix, suffix, tackon)
+        self.word = self.stem["principal parts"][self.ending["key"] - 1]
+        self.word += self.ending["ending"]
+        if self.prefix:
+            self.word = self.prefix["word"] + self.prefix.get("connect", "") + self.word
+        if self.suffix:
+            self.word = self.word + self.suffix.get("connect", "") + self.suffix["word"]
+        if self.tackon:
+            self.word += self.tackon["word"]
 
 
 class Pronoun(Word):
     def __init__(self, target, stem, ending, prefix=None, suffix=None, tackon=None):
         super().__init__(target, stem, ending, prefix, suffix, tackon)
+        assert (self.stem["which"], self.stem["variation"]) == (
+            self.ending["which"],
+            self.ending["variation"],
+        ), "The stem and ending of this word are incompatible!"
+        assert Word.gender_matches(
+            self.stem["gender"], self.ending["gender"]
+        ), "The stem and ending of this word are incompatible!"
+        self.word = self.stem["principal parts"][self.ending["key"] - 1]
+        self.word += self.ending["ending"]
+        if self.prefix:
+            self.word = self.prefix["word"] + self.prefix.get("connect", "") + self.word
+        if self.suffix:
+            self.word = self.word + self.suffix.get("connect", "") + self.suffix["word"]
+        if self.tackon:
+            self.word += self.tackon["word"]
 
 
 class Number(Word):
     def __init__(self, target, stem, ending, prefix=None, suffix=None, tackon=None):
         super().__init__(target, stem, ending, prefix, suffix, tackon)
+        assert (self.stem["which"], self.stem["variation"]) == (
+            self.ending["which"],
+            self.ending["variation"],
+        ), "The stem and ending of this word are incompatible!"
+        assert Word.gender_matches(
+            self.stem["gender"], self.ending["gender"]
+        ), "The stem and ending of this word are incompatible!"
+        self.word = self.stem["principal parts"][self.ending["key"] - 1]
+        self.word += self.ending["ending"]
+        if self.prefix:
+            self.word = self.prefix["word"] + self.prefix.get("connect", "") + self.word
+        if self.suffix:
+            self.word = self.word + self.suffix.get("connect", "") + self.suffix["word"]
+        if self.tackon:
+            self.word += self.tackon["word"]
 
 
-class Supine(Word):
+# TODO: supines are weird - they're only in the inflections, not the dictionary - check the last three assertions
+class Supine:
     def __init__(self, target, stem, ending, prefix=None, suffix=None, tackon=None):
-        super().__init__(target, stem, ending, prefix, suffix, tackon)
+        self.prefix = prefix
+        self.stem = stem
+        self.ending = ending
+        self.suffix = suffix
+        self.tackon = tackon
+        self.target = target
+        if ending:
+            assert self.stem["part of speech"] == "V" and self.ending["part of speech"] == "SUPINE", "The stem and ending of this word are incompatible!"
+        if prefix:
+            assert Word.part_of_speech_matches(
+                stem["part of speech"], prefix["from"]
+            ), "The stem and prefix of this word are incompatible!"
+        if suffix:
+            assert Word.part_of_speech_matches(
+                stem["part of speech"], suffix["from"]
+            ), "The stem and suffix of this word are incompatible!"
+        if tackon:
+            assert Word.part_of_speech_matches(
+                stem["part of speech"], tackon["with"]
+            ), "The stem and tackon of this word are incompatible!"
+        self.word = self.stem["principal parts"][self.ending["key"] - 1]
+        self.word += self.ending["ending"]
+        if self.prefix:
+            self.word = self.prefix["word"] + self.prefix.get("connect", "") + self.word
+        if self.suffix:
+            self.word = self.word + self.suffix.get("connect", "") + self.suffix["word"]
+        if self.tackon:
+            self.word += self.tackon["word"]
 
 
-class Pack(Word):
+# TODO: packs are weird - they're only in the dictionary, not the inflections - check the last three assertions
+class Pack:
     def __init__(self, target, stem, ending, prefix=None, suffix=None, tackon=None):
-        super().__init__(target, stem, ending, prefix, suffix, tackon)
+        self.prefix = prefix
+        self.stem = stem
+        self.ending = ending
+        self.suffix = suffix
+        self.tackon = tackon
+        self.target = target
+        if ending:
+            assert self.stem["part of speech"] == "PACK" and self.ending["part of speech"] == "PRON", "The stem and ending of this word are incompatible!"
+        if prefix:
+            assert Word.part_of_speech_matches(
+                stem["part of speech"], prefix["from"]
+            ), "The stem and prefix of this word are incompatible!"
+        if suffix:
+            assert Word.part_of_speech_matches(
+                stem["part of speech"], suffix["from"]
+            ), "The stem and suffix of this word are incompatible!"
+        if tackon:
+            assert Word.part_of_speech_matches(
+                stem["part of speech"], tackon["with"]
+            ), "The stem and tackon of this word are incompatible!"
+        self.word = self.stem["principal parts"][self.ending["key"] - 1]
+        self.word += self.ending["ending"]
+        if self.prefix:
+            self.word = self.prefix["word"] + self.prefix.get("connect", "") + self.word
+        if self.suffix:
+            self.word = self.word + self.suffix.get("connect", "") + self.suffix["word"]
+        if self.tackon:
+            self.word += self.tackon["word"]
+
